d12e1fed0030ee87af1f7f809f43398cc90288de
Aryan Sisodia <shrn82256@gmail.com>
2020-09-04 22:15:03 +0530

    add url to videos

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -5,9 +5,9 @@ import heartEmoji from '../assets/emojis/heart.png';
 import clapperEmoji from '../assets/emojis/clapper.png';
 
 function Videos() {
-  const renderThumbnail = (emoji, backgroundColor, title) => {
+  const renderThumbnail = (emoji, backgroundColor, title, url) => {
     return (
-      <div className="thumbnail">
+      <a className="thumbnail" href={url} target="__blank__">
         <div
           className="thumbnail-emoji"
           style={{
@@ -20,15 +20,30 @@ function Videos() {
         <div className="thumbnail-title has-text-centered has-text-light">
           {title}
         </div>
-      </div>
+      </a>
     );
   };
 
   return (
     <div id="Videos">
-      {renderThumbnail(cryLaughingEmoji, '#91ccec', 'Bloopers')}
-      {renderThumbnail(heartEmoji, '#ffc0eb', 'Gift')}
-      {renderThumbnail(clapperEmoji, 'lightgrey', 'Skit')}
+      {renderThumbnail(
+        cryLaughingEmoji,
+        '#91ccec',
+        'Bloopers',
+        'https://drive.google.com/file/d/1Y14p5zQvhYGt0q7z9FZN0Ff9rnFQg9oK/view'
+      )}
+      {renderThumbnail(
+        heartEmoji,
+        '#ffc0eb',
+        'Gift',
+        'https://drive.google.com/file/d/1CM2GmVWvYe0bBu1382wqvHumqyLWjD-a/view'
+      )}
+      {renderThumbnail(
+        clapperEmoji,
+        'lightgrey',
+        'Skit',
+        'https://drive.google.com/file/d/1zVABkAB_S16DIXAHFSezSaKNajlvuoLa/view'
+      )}
     </div>
   );
 }
